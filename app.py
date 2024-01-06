9103b0a15b8aa424bc649c68fe5fab4469a3c2df
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-06 17:33:59 +0800

    Enhancing the css

--- app.py
+++ app.py
@@ -1,33 +1,24 @@
-# import the request module from flask
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-# to get the data from the form we need to use the request object
-# lets add a methods argument to the route decorator
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # define a variable to hold the largest number
     largest = None
-    # define a variable to hold the messagee'
-    # logic:
-    # 1. check if the request method is post (basically checked if the form is submitted)
     if request.method == 'POST':
-        # 2. get the numbers data from the form with request object using request.form
+        largest = None
         num1 = float(request.form['num1'])
         num2 = float(request.form['num2'])
         num3 = float(request.form['num3'])
-        # compare the numbers to find the largest
         if num1 > num2 and num1 > num3:
             largest = num1
         elif num2 > num1 and num2 > num3:
             largest = num2
         else:
             largest = num3
-        # set the message
 
-    # render the template with the largest and the message
+    largest = format(largest, '.2f')
+
     return render_template('index.html', largest=largest)
 
 if __name__ == '__main__':
